1999421067b2f90c7d02be27e20b78dd6134be4c
Stijn Houwelijckx <stijn@houwelijckx.be>
2024-01-04 19:04:19 +0100

    Tested API request working

--- App.js
+++ App.js
@@ -1,7 +1,45 @@
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Platform } from "react-native";
+// import { Dimensions } from "react-native";
 
 export default function App() {
+  // const windowWidth = Dimensions.get("window").width;
+  // const windowHeight = Dimensions.get("window").height;
+
+  // console.log("Height: " + windowHeight + " & Width: " + windowWidth);
+
+  const [products, setProduct] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      //10.0.2.2:60856
+      //http://stienoshoes.ddev.site
+      let url;
+      if (Platform.OS == "android") {
+        //ddev describe om port number te weten te komen
+        url = "http://10.0.2.2:60856/api/catalog/";
+      } else {
+        url = "http://stienoshoes.ddev.site/api/catalog/";
+      }
+
+      const response = await fetch(url, {
+        method: "GET",
+      });
+      const json = await response.json();
+      // console.log(json.items);
+      setProduct(json.items);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  console.log(products[0]);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! Test</Text>
