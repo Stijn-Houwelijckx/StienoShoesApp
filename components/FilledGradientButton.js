113ddca0d6b0df104af6de25cb3dddaa992993af
Stijn Houwelijckx <stijn@houwelijckx.be>
2024-01-05 21:01:44 +0100

    Gradient button component

---
+++ components/FilledGradientButton.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, TouchableOpacity, StyleSheet, Text, Image } from "react-native";
+import IonIcon from "react-native-vector-icons/Ionicons";
+import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
+import { LinearGradient } from "expo-linear-gradient";
+
+const CatalogItem = (props) => {
+  //   console.log(props.shoeImage);
+  return (
+    <LinearGradient
+      colors={["#FCAD72", "#FF626D"]}
+      style={styles.button}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}
+    >
+      <Text style={styles.buttonText}>{props.buttonText}</Text>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 360,
+    paddingHorizontal: 24,
+    paddingVertical: 12,
+    width: "100%",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    // fontWeight: "bold",
+    textTransform: "uppercase",
+  },
+});
+
+export default CatalogItem;
