0e60149b491e0450f2a460c8fe54017ee32f8c21
Stijn Houwelijckx <stijn@houwelijckx.be>
2024-01-06 01:24:59 +0100

    Styled product page titlecontainer

--- screens/ProductScreen.js
+++ screens/ProductScreen.js
@@ -10,6 +10,7 @@ import {
   Button,
 } from "react-native";
 import { useFocusEffect } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
 
 const ProductScreen = ({ navigation, route }) => {
   const { id } = route.params;
@@ -41,6 +42,14 @@ const ProductScreen = ({ navigation, route }) => {
 
       const json = await response.json();
 
+      // console.log(json.productImg);
+      if (Platform.OS == "android") {
+        json.productImg = json.productImg.replace(
+          "stienoshoes.ddev.site",
+          "10.0.2.2:52951"
+        );
+      }
+
       // console.log(json.items);
       setProduct(json);
     } catch (error) {
@@ -59,27 +68,95 @@ const ProductScreen = ({ navigation, route }) => {
   //   getProduct();
   // }, []);
 
-  // console.log(products);
-
   // console.log(id);
 
+  // console.log(product.price.amount);
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.text}>{product.title}</Text>
-    </View>
+    <LinearGradient colors={["#FCAD72", "#FF626D"]} style={styles.screen}>
+      <View style={styles.imageContainer}>
+        <View style={styles.backgroundCircle}></View>
+        <Image
+          style={[styles.popularShoeImage, { width: 297, height: 186 }]}
+          source={{ uri: product.productImg }}
+        />
+      </View>
+
+      <View style={styles.titleContainer}>
+        <Text style={styles.shoeName}>{product.title}</Text>
+        <Text style={styles.shoeColor}>{product.fullColor}</Text>
+        <Text style={styles.shoePrice}>
+          € {(product.price?.amount / 100)?.toFixed(2)}
+        </Text>
+      </View>
+
+      <View style={styles.infoContainer}>
+        <Text style={styles.shoeDescription}>{product.productDescription}</Text>
+      </View>
+
+      {/* <Text style={styles.text}>{(product.price.amount / 100).toFixed(2)}</Text> */}
+
+      {/* {product.sizes?.map((size) => (
+        <Text key={size} style={styles.sizeText}>
+          {size}
+        </Text>
+      ))} */}
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    // justifyContent: "center",
+    // alignItems: "center",
     backgroundColor: "#F8F6F6",
   },
-  text: {
-    // color: "red",
-    fontSize: 28,
+  titleContainer: {
+    paddingHorizontal: 32,
+    paddingTop: 36,
+    height: 270,
+  },
+  shoeName: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    fontSize: 32,
+  },
+  shoePrice: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    fontSize: 32,
+  },
+  shoeColor: {
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+  imageContainer: {
+    position: "absolute",
+    right: 0,
+    top: 16,
+  },
+  popularShoeImage: {
+    resizeMode: "contain",
+  },
+  backgroundCircle: {
+    backgroundColor: "#333235",
+    height: 230,
+    width: 230,
+    borderRadius: 360,
+    position: "absolute",
+    left: 50,
+  },
+  infoContainer: {
+    backgroundColor: "#29282D",
+    paddingHorizontal: 16,
+    paddingTop: 28,
+    height: "100%",
+    borderRadius: 40,
+  },
+  shoeDescription: {
+    color: "#FFFFFF",
+    fontSize: 16,
   },
 
   // ======================
